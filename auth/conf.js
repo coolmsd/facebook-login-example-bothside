e1e010b130b9f91de106fe47781635170eda732c
SodamMoon <coolmsd@gmail.com>
2016-04-05 12:57:03 +1200

    added example of callback url

--- auth/conf.js
+++ auth/conf.js
@@ -2,7 +2,7 @@ module.exports = {
     facebook: {
         clientID: 'Enter your consumer key here'
       , clientSecret: 'Enter your consumer secret here'
-      , callbackURL: 'Enter your callbackURL here'
+      , callbackURL: 'Enter your callbackURL here' /* 'http://localhost:3000/facebook/callback' */
       , profileFields: ['id', 'displayName', 'photos', 'email'] /* user informations to get */
       , options: { authType: 'rerequest', scope: ['email', 'ads_management', 'ads_read'] } /* permissions */
     }

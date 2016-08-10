cf47f9e0dce94586471f22c68809b69f800a871a
SodamMoon <coolmsd@gmail.com>
2016-08-10 17:12:28 +1200

    testData

--- auth/conf.js
+++ auth/conf.js
@@ -1,17 +1,17 @@
 module.exports = {
     facebook: {
-        clientID: 'Enter your consumer key here'
-      , clientSecret: 'Enter your consumer secret here'
-      , callbackURL: 'Enter your callbackURL here' /* 'http://localhost:3000/facebook/callback' */
+        clientID: '206197459739676'
+      , clientSecret: '1a277934fbfab4c4021434568316e1d1'
+      , callbackURL: 'http://localhost:8090/facebook/callback'
       , profileFields: ['id', 'displayName', 'photos', 'email'] /* user informations to get */
-      , options: { authType: 'rerequest', scope: ['email', 'ads_management', 'ads_read'] } /* permissions */
-    }
-  , instagram: {
-        clientId: 'Enter your consumer key here'
-      , clientSecret: 'Enter your consumer secret here'
-    }
-  , google: {
-        clientId: 'Enter your consumer key here'
-      , clientSecret: 'Enter your consumer secret here'
-    }
+      , options: { authType: 'rerequest', scope: ['public_profile', 'ads_management', 'ads_read', 'publish_pages', 'manage_pages'] } /* permissions */
+      , adAccountId: '482898265106052'  /* add account Id*/
+      , adsAPIVersion: 'v2.7'
+      , accessToken_Test_community : 'EAAC7iRZAXZABwBAIIT0qhHLawnv3uXZC57WoYcM7T3ZCwEHArQHrtS13QmuDMupjZC6gzbW48cX3eo1cUGPOp5GXJ2OJdsT7MVFMKTCQXKaY1cLK3VsPfeuq9e8i2LFIrdOlb937WArwzeVVyVJQHWEwZCzjgMGwhdVN0yW1YxuQZDZD'
+      , accessToken_NuCom: 'EAAC7iRZAXZABwBALSXaZBXFi2IMsXRQLic9eRZAhNtUede0kzKpqUV9a3bFdE6rk1wSmZAh0Ihitx64pBsSGpR9Bm5Ax7Mw2DfEl48Uoh81kWBBZCOmZBOZAxTG8CZAuqpN755Sq9hg6F4vLlzQkm9ZBZAcfzHhFvNPBvo7C9VxYFgYBAZDZD'
+      , accessToken_Cafedori: 'EAAC7iRZAXZABwBAC0tg28vr8mthg22vt0EE96pXDq35vMmHFoY2FNuPq6vupR4PjiTxvKI2EuJVgdC3fdJw7QpMbNWnKPq9glnfBahOZCgV99OgM3mwHKN7r59A0DatFtPKNSIuWiVEK54IZBqSAiP5OOkZB6bed8KNsqZAJzV7gZDZD'
+      , accessToken: 'EAAC7iRZAXZABwBAIC8nZCbBJlAIyU4yAFg2XniD5ygg8puPr25PWnX8CfOBH0GkiWsn582nbmvzZCISjVfXO6ATmyU4kS0Jqj5FUhvkWZAjZAMSRLg0tWpgOCZCcxzTdTkposjS94ZAkEtAUDOCzRSH9uy27KxrD6HoZD'
+  // /* app-token */ accessToken: '206197459739676|euY1DmO9EreTjLEXttOLsK43jds',
+    },
+
 };
